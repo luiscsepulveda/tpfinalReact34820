0e6c6b346631e58d41599d223cc1aa154ef0c986
Luis C. Sepulveda <luiscsepulveda.dev@gmail.com>
2022-12-07 23:45:55 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+
+import './App.css';
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+
+function App() {
+  return (
+    <div className="App">
+  
+      <BrowserRouter>
+        <Routes>
+          <Route path='/'/>
+          <Route path='/category/:categoryId' />
+          <Route path='/detail/:productId' /> 
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
